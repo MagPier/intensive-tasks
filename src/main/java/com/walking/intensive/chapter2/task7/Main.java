fde242ae9b902d73192eeebd3a4edd95fd562883
Magpier <garson.ao@gmail.com>
2023-11-30 20:30:17 +0300

    Chapter2 Task 7

--- src/main/java/com/walking/intensive/chapter2/task7/Main.java
+++ src/main/java/com/walking/intensive/chapter2/task7/Main.java
@@ -8,16 +8,18 @@ public class Main {
         System.out.println(friendNumber(300));
     }
 
-    static int friendNumber(int N) {
-        if (N < 0 || N > 1000000) {
+    static int friendNumber(int n) {
+        if (n < 0 || n > 1000000) {
             System.out.println("Число вне диапазона");
             return 0;
         }
-        for (int i = N; i >= 1; i--) {
+
+        for (int i = n; i >= 1; i--) {
             if (calculateNumber(calculateNumber(i)) == i) {
                 return i;
             }
         }
+
         return 0;
     }
 
@@ -27,7 +29,6 @@ public class Main {
             if (cycle % i == 0 && number != i) {
                 number += i;
             }
-
         }
         return number;
     }
